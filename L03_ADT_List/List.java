467c42d666b92b36da1a0480295248c58279605b
Osamah Sarraj <osamah.sarraj@gmail.com>
2023-11-09 12:05:30 +0300

    adding ADT List Specification

--- L03_ADT_List/List.java
+++ L03_ADT_List/List.java
@@ -1,23 +1,5 @@
 package L03_ADT_List;
 
-/*
- * a brief explanation of each method in the `List` interface:
- *
- * 1. `empty()`: Checks if the list is empty.
- * 2. `full()`: Checks if the list is at maximum capacity.
- * 
- * 3. `last()`: Checks if the cursor is at the last element.
- * 
- * 4. `findFirst()`: Moves the cursor to the first element.
- * 5. `findNext()`: Moves the cursor to the next element.
- * 
- * 6. `retrieve()`: Gets the element at the cursor's position.
- * 7. `update(T e)`: Updates the element at the cursor's position.
- * 
- * 8. `insert(T e)`: Inserts a new element at the cursor's position.
- * 9. `remove()`: Removes the element at the cursor's position.
- */
-
 
 public interface List<T> {
     public boolean empty();
@@ -33,4 +15,63 @@ public interface List<T> {
 
     public void insert(T e);
     public void remove();
+
+
+/*
+ * ADT List Specification: 
+ * Operations: All operations act on a list L.
+ *
+ * 1. empty(boolean flag): 
+ *    - Input: None.
+ *    - Results: Sets flag to true if L is empty, otherwise false.
+ *    - Output: flag.
+ * 
+ * 2. full(boolean flag): 
+ *    - Input: None.
+ *    - Returns: Sets flag to true if L is full, otherwise false.
+ *    - Output: flag.
+ * 
+ * 3. last(boolean flag): 
+ *    - Input: None.
+ *    - Requires: L is not empty.
+ *    - Results: Sets flag to true if the current element is the last element, otherwise false.
+ *    - Output: flag.
+ * 
+ * 4. findFirst(): 
+ *    - Requires: L is not empty.
+ *    - Input: None.
+ *    - Results: Sets the first element as the current element.
+ *    - Output: None.
+ *
+ * 5. findNext(): 
+ *    - Requires: L is not empty and current is not the last element.
+ *    - Input: None.
+ *    - Results: Sets the element following the current element as the current element.
+ *    - Output: None.
+ *
+ * 6. retrieve(Type e): 
+ *    - Requires: L is not empty.
+ *    - Input: None.
+ *    - Results: Copies the current element into e.
+ *    - Output: The element e.
+ *
+ * 7. update(Type e): 
+ *    - Requires: L is not empty.
+ *    - Input: e.
+ *    - Results: Copies element e into the current node.
+ *    - Output: None.
+ *
+ * 8. insert(Type e): 
+ *    - Requires: L is not full.
+ *    - Input: e.
+ *    - Results: Inserts a new node containing e after the current element. If L is empty, also sets e as the head.
+ *    - Output: None.
+ *
+ * 9. remove(): 
+ *    - Requires: L is not empty.
+ *    - Input: None.
+ *    - Results: Removes the current element. Sets current to NULL if L is empty, or to the successor of the deleted element, or to the first element if there is no successor.
+ *    - Output: None.
+ */
+
 }

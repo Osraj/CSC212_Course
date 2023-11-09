fcc31dda2578d5cbe6b4b1894225e416076fb61f
Osamah Sarraj <osamah.sarraj@gmail.com>
2023-11-09 11:36:13 +0300

    Correcting the methods

--- L02_Recursion/Linear_vs_Binary_Recursion.java
+++ L02_Recursion/Linear_vs_Binary_Recursion.java
@@ -22,16 +22,26 @@ public class Linear_vs_Binary_Recursion {
             return 0;
         }
         // Recursive case: Add the first element to the sum of the rest of the array.
-        return A[0] + sumLinearRecursive(A, n - 1);
+        return A[n - 1] + sumLinearRecursive(A, n - 1);
     }
 
-    // Recusrive function to calculate the sum of array A using binary_recursive
-    public static int sumBinaryRecursive(int[] A, int i, int n) {
-        // Base case: When n is 0, the sum is 0.
-        if (n == 0) {
+    // Recursive function to calculate the sum of array A using binary recursion
+    public static int sumBinaryRecursive(int[] A, int start, int end) {
+        // Base case: if the start index is greater than or equal to the end index,
+        // it means we have covered all elements, so return 0
+        if (start >= end) {
             return 0;
         }
-        // Recursive case: Add the first element to the sum of the rest of the array.
-        return A[i] + sumBinaryRecursive(A, i + 1, n - 1);
+        // Another base case: if there is only one element between start and end,
+        // return that element
+        else if (start == end - 1) {
+            return A[start];
+        } else {
+            // Calculate the mid-point of the current array segment
+            int mid = (start + end) / 2;
+            // Recursively sum the first half from start to mid, and the second half from
+            // mid to end, and return the total sum of both halves
+            return sumBinaryRecursive(A, start, mid) + sumBinaryRecursive(A, mid, end);
+        }
     }
 }
